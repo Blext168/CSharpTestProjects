3caf8e6b29c67f5eb7a907cf4949f6a4a59de1b5
Danilo Schiefer <danilo.schiefer@lit.de>
2025-05-28 12:27:20 +0200

    Add DatabaseSQLTester project with SQLite integration
    Introduced a new project, `DatabaseSQLTester`, featuring SQLite integration using Entity Framework Core. This includes models, a DbContext for handling database interactions, test data initialization, and a console-based SQL query interface.

---
+++ DatabaseSQLTester/HandelsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DatabaseSQLTester.Model;
+using Microsoft.EntityFrameworkCore;
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+namespace DatabaseSQLTester;
+
+public class HandelsDbContext : DbContext
+{
+    public DbSet<Artikel> Artikel { get; set; }
+    public DbSet<Lager> Lager { get; set; }
+    public DbSet<Verkauf> Verkaufe { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=handel.db");
+    }
+}
---
+++ DatabaseSQLTester/Model/Lager.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseSQLTester.Model;
+
+public class Lager
+{
+    public int LagerId { get; init; }
+    public int ArtikelId { get; init; }
+    public int Bestand { get; init; }
+    public string? Lagerort { get; init; }
+    public DateTime LetzteInventur { get; init; }
+}
