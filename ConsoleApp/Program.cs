17ef5531685d0f7a612ea6987f36ef568a46c43e
Danilo Schiefer <danilo.schiefer@lit.de>
2025-12-16 14:39:38 +0100

    Added Console Test Project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp
+{
+    public sealed class Program()
+    {
+
+        static void Main(string[] args)
+        {
+            string test = Enum.ToObject(typeof(SampleEnum), 2).ToString() ?? string.Empty;
+            Console.WriteLine("EnumValue: " + test);
+        }
+    }
+}
